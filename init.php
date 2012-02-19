9ee29d4e6a195c719aa1c4865b580d8093cc05fe
homm <homm86@gmail.com>
2012-02-19 20:43:34 +0600

    Путь до репозитория изменен.

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load the Twig class autoloader
-require_once Kohana::find_file('vendor', 'Twig/lib/Twig/Autoloader');
+require DOCROOT.'../../vendor/twig-1.6/lib/Twig/Autoloader.php';
 
 // Register the Twig class autoloader
 Twig_Autoloader::register();
