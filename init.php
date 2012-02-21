930881edd0d2441ec2bd2766a34ab87c8f25c9ae
homm <homm86@gmail.com>
2012-02-22 00:38:04 +0600

    Twig autoloader moved to Kohana_Twig. Now it loads on demand.

--- init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// Load the Twig class autoloader
-require DOCROOT.'../../vendor/twig-1.6/lib/Twig/Autoloader.php';
-
-// Register the Twig class autoloader
-Twig_Autoloader::register();
