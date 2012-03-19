eff0d87710d8dc6c5990df62ea1fab9cd1dfda8a
homm <homm86@gmail.com>
2012-03-19 16:35:19 +0600

    Версия Твига обновлена.

--- classes/kohana/twig.php
+++ classes/kohana/twig.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load the Twig class autoloader
-require DOCROOT.'../../vendor/twig-1.6.1/lib/Twig/Autoloader.php';
+require DOCROOT.'../../vendor/twig-1.6.2/lib/Twig/Autoloader.php';
 
 // Register the Twig class autoloader
 Twig_Autoloader::register();
