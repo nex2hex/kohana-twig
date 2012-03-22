78fc30aaf3809c34316eccb5df679d790d1b92d3
homm <homm86@gmail.com>
2012-03-22 10:27:12 +0600

    Автоллоадер теперь подключается в Kohana_Twig_Environment.

--- classes/kohana/twig/environment.php
+++ classes/kohana/twig/environment.php
@@ -1,5 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// Load the Twig class autoloader
+require DOCROOT.'../../vendor/twig-1.6.2/lib/Twig/Autoloader.php';
+
+// Register the Twig class autoloader
+Twig_Autoloader::register();
+
 /**
  * Twig Loader
  *
